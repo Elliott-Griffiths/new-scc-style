237e90d7227bfc65c17876f2f772550da0e08f07
elliott.griffiths.86 <elliott.griffiths.86@gmail.com>
2025-07-17 21:05:08 +0000

    updateing script file

--- main.js
+++ main.js
@@ -1,160 +1,3 @@
-
-// --- VARIABLES ------------------------------------------------------------ \\
-
-
-
-// --- FORM LOAD ------------------------------------------------------------ \\
-
-handleInitialisingEvent();
-
-// --- FORM READY ----------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_ready')
-  .on('_KDF_ready', function (event, kdf) {
-    handleOnReadyEvent(event, kdf);
-  });
-
-// --- PAGE CHANGES --------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_pageChange')
-  .on('_KDF_pageChange', function (event, kdf, currentpageid, targetpageid) {
-    handlePageChangeEvent(event, kdf, currentpageid, targetpageid);
-    
-    // Toggle back button visibility
-    displayBackButton(targetpageid > 1 && kdf.form.complete !== "Y");
-});
-
-// --- FIELD CHANGES -------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_fieldChange')
-  .on('_KDF_fieldChange', function (event, kdf, field) {
-    handleFieldChangeEvent(event, kdf, field);
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_optionSelected')
-  .on('_KDF_optionSelected', function (event, kdf, field, label, val) {
-    handleOptionSelectedEvent(event, kdf, field, label, val);
-    
-    if (field === 'sel_notice_banner') {
-      KDF.hideMessages();
-      if (val === 'Information') {
-        KDF.showInfo('This is the information notice banner');
-      }
-      if (val === 'Success') {
-        KDF.showSuccess('This is the success notice banner');
-      }
-      if (val === 'Warning') {
-        KDF.showWarning('This is the warning notice banner');
-      }
-      if (val === 'Error') {
-        KDF.showError('This is the red error banner');
-      }
-    }
-    
-    if (field === "rad_chow_conditional") {
-        hideShowElement("area_conditional", val);
-    }
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_rowSelected')
-  .on('_KDF_rowSelected', function (event, kdf, tableid, row) {
-    
-});
-
-// --- MAP ACTIONS ---------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_mapReady')
-  .on('_KDF_mapReady', function (event, kdf, type, name, map, positionLayer, markerLayer, marker, projection) {
-    handleMapReadyEvent(event, kdf, type, name, map, positionLayer, markerLayer, marker, projection);
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_mapClicked')
-  .on('_KDF_mapClicked', function (event, kdf, type, name, map, positionLayer, markerLayer, marker, lat, lon, plat, plon) {
-    handleMapClickEvent(event, kdf, type, name, map, positionLayer, markerLayer, marker, lat, lon, plat, plon);
-});
-
-$('#dform_zz_demo_form')
-  .off('_Selected_Layer')
-  .on('_Selected_Layer', function(event, kdf, layerName, layerAttributes) {
-    handleSelectedMapLayerEvent(event, kdf, layerName, layerAttributes);
-});
-
-$("#dform_zz_demo_form")
-  .off("_KDF_clearAttribute")
-  .on("_KDF_clearAttribute", function (event, kdf) {
-    handleClearMapFieldsEvent(event, kdf);
-});
-
-// --- OBJECT --------------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_objectidSet')
-  .on('_KDF_objectidSet', function (event, kdf, type, id) {
-    handleObjectIdSet(event, kdf, type, id);
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_objectdataLoaded')
-  .on('_KDF_objectdataLoaded', function (event, kdf, response, type, id) {
-    handleObjectIdLoaded(event, kdf, response, type, id);
-});
-
-// --- CUSTOM ACTIONS ------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_custom')
-  .on('_KDF_custom', function (event, kdf, response, action, actionedby) {
-    handleSuccessfulAction(event, kdf, response, action, actionedby);
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_customError')
-  .on('_KDF_customError', function (event, customaction, xhr, settings, thrownError) {
-    handleFailedAction(event, customaction, xhr, settings, thrownError);
-});
-
-// --- SAVE FORM ------------------------------------------------------------ \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_save')
-  .on('_KDF_save', function (event, kdf) {
-    handleFormSave(event, kdf);
-});
-
-$('#dform_zz_demo_form')
-  .off('_KDF_saveError')
-  .on('_KDF_saveError', function (event, kdf) {
-    handleFailedSave();
-});
-
-// --- COMPLETE FORM -------------------------------------------------------- \\
-
-$('#dform_zz_demo_form')
-  .off('_KDF_complete')
-  .on('_KDF_complete', function (event, kdf) {
-    handleFomComplate(event, kdf);
-});
-
-
-// --- FUNCTIONS ------------------------------------------------------------ \\
-
-
-
-
-
-
-
-
-
-
-
 function logArguments(event, kdf, ...args) {
   console.group(event.type ? event.type : "event");
   console.log("event", event);
@@ -174,9 +17,9 @@ let pageName = "";
 let addressSearchType = {};
 let acceptGMSites = true;
 
-const defaultDateMessage = "Enter the date in the correct format";
-
 const datePairs = [];
+
+const relatedServices = [];
 
 let fieldsToCheckBeforeClose = [];
 
@@ -402,7 +245,7 @@ function handleInitialisingEvent() {
 function handleOnReadyEvent(_, kdf) {
   customerState = kdf.customerset;
 
-  // --- ADD CONTENT TO WHY WE NEED DATE OF BIRTH --------------------------- \\
+  // --- ADD CONTENT TO WHY WE NEED DATE OF BIRTH -------------------------- \\
 
   $(".dob-reason").text(
     "Your date of birth is a helpful way to confirm your identity and protect your information."
@@ -410,6 +253,10 @@ function handleOnReadyEvent(_, kdf) {
   $(".their-dob-reason").text(
     "Their date of birth is a helpful way to confirm their identity and protect their information."
   );
+
+  // --- BUILD RELATED SERVICES -------------------------------------------- \\
+  
+  buildRelatedServiceCards(relatedServices, 'related-services-panel');
 
   // --- REMOVE TAB INDEX FROM SELECT ELEMENTS ----------------------------- \\
 
@@ -905,6 +752,46 @@ function handleOnReadyEvent(_, kdf) {
       if (e.type === "input") inputDate(this.id, null, e.which);
       if (e.type === "focusout") handleDateValidation(parentId, this);
     });
+
+// --- CUSTOM TIME FUNCTIONS ------------------------------------------------ \\
+
+  $('.time-hour, .time-minute').on('input', function (e) {
+    const nextFieldId = $(this).hasClass('time-hour') ?
+      'dform_widget_num_incident_occured_minute' :
+      'dform_widget_sel_incident_occured_ampm';
+    inputTime(this.id, nextFieldId);
+  });
+  
+  $('.time-ampm').on('change blur', function () {
+    const parentId = $(this).closest('.time-field').attr('id');
+    const hourVal = $('#dform_widget_num_incident_occured_hour').val();
+    const minuteVal = $('#dform_widget_num_incident_occured_minute').val();
+    const ampm = $(this).val();
+  
+    const hour = (hourVal !== '' && hourVal !== null && hourVal !== undefined) ? parseInt(hourVal, 10) : NaN;
+    const minute = (minuteVal !== '' && minuteVal !== null && minuteVal !== undefined) ? parseInt(minuteVal, 10) : NaN;
+  
+    handleTimeValidation(parentId, hour, minute, ampm);
+  });
+  
+  $('.time-field').on('focusout', function (e) {
+    if (!$(this).has(e.relatedTarget).length && e.relatedTarget !== this) {
+      const parentId = $(this).attr('id');
+      const hourVal = $('#dform_widget_num_incident_occured_hour').val();
+      const minuteVal = $('#dform_widget_num_incident_occured_minute').val();
+      const ampm = $('#dform_widget_sel_incident_occured_ampm').val();
+  
+      const hour = (hourVal !== '' && hourVal !== null && hourVal !== undefined) ? parseInt(hourVal, 10) : NaN;
+      const minute = (minuteVal !== '' && minuteVal !== null && minuteVal !== undefined) ? parseInt(minuteVal, 10) : NaN;
+  
+      handleTimeValidation(parentId, hour, minute, ampm);
+    }
+  });
+  
+  // Add a listener to the native HTML5 time input to display its value
+  $('#dform_widget_time_incident_occured[type="time"]').on('input change', function () {
+    $('#display_formatted_24hr').text($(this).val());
+  });
 
   // --- HANDLE KEYUP EVENTLISTENER FOR CHECK PROGRESS --------------------- \\
 
@@ -2531,6 +2418,167 @@ function validDate(id, day, month, year, activeField, baseMessage) {
 }
 
 
+// --- CUSTOM TIME FUNCTIONS ------------------------------------------------ \\
+
+/**
+ * Handles the validation logic for the time input fields.
+ * @param {string} parentId - The base ID for the time field container.
+ * @param {number} hour - The hour value.
+ * @param {number} minute - The minute value.
+ * @param {string} ampm - The AM/PM value.
+ */
+function handleTimeValidation(parentId, hour, minute, ampm) {
+    console.log(parentId)
+    const txtFieldId = parentId.replace("_time_", "_txt_");
+    const timeMessage = getValidationMessageFromSession(txtFieldId);
+  
+    $(`#${parentId} .time-hour, #${parentId} .time-minute, #${parentId} .time-ampm`).removeClass('dform_fielderror');
+    $(`#${parentId}`).find('.dform_validationMessage').text(timeMessage).hide();
+  
+    let hasError = false;
+    let errorMsg = "";
+    const errorFields = [];
+    const timeFields = ['time-hour', 'time-minute', 'time-ampm'];
+  
+    const errorConditions = [
+      {
+        condition: isNaN(hour) && isNaN(minute) && !ampm,
+        message: timeMessage,
+        fields: timeFields
+      },
+      {
+        condition: !isNaN(hour) && !isNaN(minute) && !ampm,
+        message: "The time you entered must include am or pm",
+        fields: ['time-ampm']
+      },
+      {
+        condition: !isNaN(hour) && isNaN(minute) && ampm,
+        message: "The time you entered must include the minute",
+        fields: ['time-minute']
+      },
+      {
+        condition: isNaN(hour) && !isNaN(minute) && ampm,
+        message: "The time you entered must include the hour",
+        fields: ['time-hour']
+      },
+      {
+        condition: !isNaN(hour) && isNaN(minute) && !ampm,
+        message: "The time you entered must include the minute and am or pm",
+        fields: ['time-minute', 'time-ampm']
+      },
+      {
+        condition: isNaN(hour) && !isNaN(minute) && !ampm,
+        message: "The time you entered must include am or pm",
+        fields: ['time-hour', 'time-ampm']
+      },
+      {
+        condition: isNaN(hour) && isNaN(minute) && ampm,
+        message: "The time you entered must include the hour and minute",
+        fields: ['time-hour', 'time-minute']
+      },
+      {
+        condition: hour > 12 || hour < 1,
+        message: "Hour must be between 1 and 12.",
+        fields: ['time-hour']
+      },
+      {
+        condition: minute > 59 || minute < 0,
+        message: "Minute must be between 00 and 59.",
+        fields: ['time-minute']
+      },
+      {
+        condition: (isNaN(hour) || isNaN(minute) || !ampm),
+        message: timeMessage,
+        fields: timeFields
+      }
+    ];
+  
+    for (const condition of errorConditions) {
+      if (condition.condition) {
+        hasError = true;
+        errorMsg = condition.message;
+        errorFields.push(...condition.fields);
+        break;
+      }
+    }
+  
+    if (hasError) {
+      errorFields.forEach(field => {
+        $(`#${parentId} .${field}`).addClass('dform_fielderror');
+      });
+      $(`#${parentId}`).find('.dform_validationMessage').text(errorMsg).show();
+      $(`#dform_widget_txt_incident_occured`).val('');
+      $(`#dform_widget_time_incident_occured[type="time"]`).val('');
+  
+      $('#display_formatted_ampm').text('N/A');
+      $('#display_formatted_24hr').text('N/A');
+      return;
+    }
+  
+    if (hour !== null && !isNaN(hour) && minute !== null && !isNaN(minute) && ampm) {
+      const formattedAmpm = formatTimeInput(hour, minute, ampm);
+      const formatted24hr = formatTimeForSubmission(hour, minute, ampm);
+  
+      $(`#dform_widget_txt_incident_occured`).val(formattedAmpm);
+      $(`#dform_widget_time_incident_occured[type="time"]`).val(formatted24hr);
+  
+      $('#display_formatted_ampm').text(formattedAmpm);
+      $('#display_formatted_24hr').text(formatted24hr);
+    }
+    $(`#dform_widget_txt_incident_occured`).change();
+    $(`#dform_widget_time_incident_occured[type="time"]`).change();
+  }
+  
+  /**
+   * Handles auto-tabbing to the next input field when max length is reached.
+   * @param {string} id - The ID of the current input field.
+   * @param {string} nextID - The ID of the next input field to focus on.
+   */
+  function inputTime(id, nextID) {
+    const maxLength = $(`#${id}`).attr('maxlength');
+    let value = $(`#${id}`).val();
+  
+    if (value.length >= maxLength) {
+      $(`#${id}`).val(value.substring(0, maxLength));
+      if (nextID) {
+        $(`#${nextID}`).focus();
+      } else {
+        $(`#${id}`).blur();
+      }
+    }
+  }
+  
+  /**
+   * Formats the time for display in the hidden text input (e.g., "9:15am").
+   * @param {number} hour - The hour (1-12).
+   * @param {number} minute - The minute (0-59).
+   * @param {string} ampm - "AM" or "PM".
+   * @returns {string} The formatted time string.
+   */
+  function formatTimeInput(hour, minute, ampm) {
+    const paddedMinute = minute.toString().padStart(2, '0');
+    return `${hour}:${paddedMinute}${ampm.toLowerCase()}`;
+  }
+  
+  /**
+   * Formats the time for submission (24-hour format, e.g., "09:15" or "15:25").
+   * @param {number} hour - The hour (1-12).
+   * @param {number} minute - The minute (0-59).
+   * @param {string} ampm - "AM" or "PM".
+   * @returns {string} The formatted time string in 24-hour format.
+   */
+  function formatTimeForSubmission(hour, minute, ampm) {
+    let hours24 = hour;
+    if (ampm === 'PM' && hour !== 12) {
+      hours24 += 12;
+    } else if (ampm === 'AM' && hour === 12) {
+      hours24 = 0; // 12 AM is 00 in 24-hour format
+    }
+    const paddedHour = hours24.toString().padStart(2, '0');
+    const paddedMinute = minute.toString().padStart(2, '0');
+    return `${paddedHour}:${paddedMinute}`;
+  }
+
 // --- PROGRESS BAR --------------------------------------------------------- \\
 
 function updateProgressBar(currentPageIndex) {
@@ -2946,7 +2994,7 @@ function showContactTeamPanel() {
     if (target) {
       target.after(contactInfo);
     } else {
-      console.error("Element with class title-container not found");
+    //   console.error("Element with class title-container not found");
     }
   }
 }
@@ -4512,7 +4560,7 @@ function storeDefaultValidationMessages() {
   const fieldClasses = [
     "address-search",
     "date-field",
-    "dform_widget_type_time",
+    "time-field",
   ];
 
   fieldClasses.forEach((className) => addValidationMessageToSession(className));
@@ -4556,3 +4604,71 @@ function getValidationMessageFromSession(id) {
     return validationMessages[id] || "Validation message not found";
   }
 }
+
+// --- RELATED SERVIES ------------------------------------------------------ \\
+
+/**
+ * Dynamically builds and inserts "related service" cards into a specified container.
+ *
+ * @param {Array<Object>} servicesData - An array of objects, where each object
+ * represents a service card and should have the following properties:
+ * - {string} headline: The main headline for the card.
+ * - {string} description: The descriptive text for the card.
+ * - {string} url: The URL the card links to.
+ * @param {string} containerId - The ID of the HTML element where the cards will be inserted.
+ */
+function buildRelatedServiceCards(servicesData, containerId) {
+    const container = document.getElementById(containerId);
+
+    if (!container) {
+        console.error(`Container with ID '${containerId}' not found.`);
+        return;
+    }
+    
+    if (servicesData && servicesData.length > 0) {
+
+    let relatedServicesMenu = container.querySelector('.related-services-menu');
+    if (!relatedServicesMenu) {
+        const h2 = document.createElement('h2');
+        h2.textContent = 'Related services';
+        container.appendChild(h2);
+
+        relatedServicesMenu = document.createElement('div');
+        relatedServicesMenu.classList.add('related-services-menu');
+        container.appendChild(relatedServicesMenu);
+    } else {
+        relatedServicesMenu.innerHTML = '';
+    }
+
+
+    if (servicesData.length === 0) {
+        console.warn('No service data provided to build cards.');
+        return; // Exit if no data
+    }
+
+    servicesData.forEach(service => {
+        const cardLink = document.createElement('a');
+        cardLink.href = service.url || '#';
+        cardLink.classList.add('related-services-card');
+
+        const headline = document.createElement('h3');
+        headline.textContent = service.headline;
+
+        const description = document.createElement('p');
+        description.textContent = service.description;
+
+        const svg = `
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+            </svg>
+        `;
+
+        cardLink.appendChild(headline);
+        cardLink.appendChild(description);
+        cardLink.insertAdjacentHTML('beforeend', svg);
+
+        relatedServicesMenu.appendChild(cardLink);
+    });
+    }
+}
+
