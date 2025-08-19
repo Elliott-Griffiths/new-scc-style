93d36d9a67ba4d6a28ae4a6d605534746ee3b644
elliott.griffiths.86 <elliott.griffiths.86@gmail.com>
2025-08-19 18:40:51 +0000

    repositioning function

--- format.js
+++ format.js
@@ -1,101 +1,98 @@
+// --- VARIABLES ------------------------------------------------------------ \\
+
+const validateAccountDetails = (details) => {
+  const requiredKeys = [
+    'CPE-token',
+    'userType',
+    'title',
+    'forename',
+    'surname',
+    'dateOfBirth',
+    'email',
+    'phone',
+    'property',
+    'streetName',
+    'city',
+    'postcode',
+    'uprm'
+  ];
+
+  for (const key of requiredKeys) {
+    const value = details[key];
+    if (value === null || typeof value === 'undefined' || value === '') {
+      return false;
+    }
+
+    switch (key) {
+      case 'email':
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(value)) {
+          return false;
+        }
+        break;
+      case 'dateOfBirth':
+        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+        if (!dateRegex.test(value) || isNaN(new Date(value))) {
+          return false;
+        }
+        break;
+      case 'postcode':
+        const postcodeRegex = /^[A-Z]{1,2}[0-9][A-Z0-9]?\s?[0-9][A-Z]{2}$/i;
+        if (!postcodeRegex.test(value)) {
+          return false;
+        }
+        break;
+      case 'phone':
+        const phoneRegex = /^\+?[0-9\s()-.]{7,25}$/;
+        if (!phoneRegex.test(value)) {
+          return false;
+        }
+        break;
+    }
+  }
+  return true;
+};
+
 // --- FORM LOAD ------------------------------------------------------------ \\
 
 handleInitialisingEvent();
 
 // --- FORM READY ----------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_ready')
   .on('_KDF_ready', function (event, kdf) {
     handleOnReadyEvent(event, kdf);
-
-    const updateProfileField = (elementId, changeLinkId, removeLinkId, data, isSecure = false, isAddress = false, isRestrictedChange = false) => {
-      const valueElement = document.getElementById(elementId);
-      const changeLink = document.getElementById(changeLinkId);
-      const removeLink = removeLinkId ? document.getElementById(removeLinkId) : null;
-
-      const hasValue = data !== null && typeof data !== 'undefined' && data !== '';
-
-      if (!hasValue) {
-        valueElement.textContent = "Not provided";
-        changeLink.textContent = "Add";
-        if (removeLink) {
-          removeLink.style.display = 'none';
-        }
-      } else {
-        if (isSecure) {
-          valueElement.textContent = '••••••••••••';
-        } else if (isAddress) {
-          valueElement.innerHTML = data;
-        } else {
-          valueElement.textContent = data;
-        }
-
-        if (isRestrictedChange) {
-          changeLink.style.display = 'none';
-          if (removeLink) {
-            removeLink.style.display = 'inline-block';
-          }
-        } else {
-          changeLink.textContent = "Change";
-          changeLink.style.display = 'inline-block';
-          if (removeLink) {
-            removeLink.style.display = 'inline-block';
-          }
-        }
-      }
-    };
-
-    const formatTitleCase = (str) => {
-      if (!str) return '';
-      return str.replace(/\b(\w)/g, char => char.toUpperCase());
-    };
-
-    const buildAddressMarkup = (address) => {
-      return `${address.property}, ${address.streetName}<br>${address.city},<br>${address.postcode}`;
-    };
-
-    const formatDateTime = (dateString) => {
-      if (!dateString) return null;
-      const dateObject = new Date(dateString);
-      return {
-        uk: {
-          date: dateObject.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' })
-        }
+    // $('#redirectUrl').attr("href", KDF.getParams()['redirectUrl']);
+    $('#dform_widget_button_but_submit_about_you').click(function () {
+      const accountDetails = {
+        'CPE-token': KDF.getVal('CPE-token'),
+        'userType': KDF.getVal('userType'),
+        'title': KDF.getVal('sel_title'),
+        'forename': KDF.getVal('txt_firstname'),
+        'surname': KDF.getVal('txt_surname'),
+        'dateOfBirth': KDF.getVal('dt_date_of_birth'),
+        'email': KDF.getVal('eml_address'),
+        'phone': KDF.getVal('tel_phone_number'),
+        'property': KDF.getVal('txt_property_about_you'),
+        'streetName': KDF.getVal('dform_widget_txt_street_name_about_you'),
+        'city': KDF.getVal('txt_city_about_you'),
+        'postcode': KDF.getVal('txt_postcode_about_you'),
+        'uprm': KDF.getVal('txt_uprn_about_you')
       };
-    };
 
-    updateProfileField('name-value', 'name-change-link', null, kdf.profileData['profile-FullName']);
-    updateProfileField('phone-value', 'phone-change-link', null, kdf.profileData['profile-TelephoneNumber']);
-    updateProfileField('email-value', 'email-change-link', null, kdf.profileData['profile-Email']);
-
-    updateProfileField('password-value', 'password-change-link', 'password-remove-link', '••••••••••••', true);
-
-    const formattedAddress = {
-      property: formatTitleCase(kdf.profileData['profile-AddressNumber']),
-      streetName: formatTitleCase(kdf.profileData['profile-AddressLine1']),
-      city: kdf.profileData['profile-AddressLine4'],
-      postcode: kdf.profileData['profile-Postcode'],
-    };
-    const fullAddress = buildAddressMarkup(formattedAddress);
-    updateProfileField('address-value', 'address-change-link', null, fullAddress, false, true);
-
-    updateProfileField('ni-value', 'ni-change-link', 'ni-remove-link', kdf.profileData['profile-NationalInsuranceNumber'], true);
-    updateProfileField('passport-value', 'passport-change-link', 'passport-remove-link', kdf.profileData['profile-PassportNumber'], true);
-    updateProfileField('nhs-value', 'nhs-change-link', 'nhs-remove-link', kdf.profileData['profile-HealthNumber'], true);
-    updateProfileField('driving-licence-value', 'driving-licence-change-link', 'driving-licence-remove-link', kdf.profileData['profile-DrivingLicenceNumber'], true);
-
-    updateProfileField('dob-value', 'dob-change-link', 'dob-remove-link', formatDateTime(kdf.profileData['profile-DateOfBirth'])?.uk.date, false, false, true);
-    updateProfileField('birth-place-value', 'birth-place-change-link', 'birth-place-remove-link', kdf.profileData['profile-PlaceOfBirth'], false, false, true);
-    updateProfileField('nationality-value', 'nationality-change-link', 'nationality-remove-link', kdf.profileData['profile-Nationality']);
-    updateProfileField('language-value', 'language-change-link', 'language-remove-link', kdf.profileData['profile-LanguagePreference']);
-    updateProfileField('status-value', 'status-change-link', 'status-remove-link', kdf.profileData['profile-MaritalStatus']);
-    updateProfileField('occupation-value', 'occupation-change-link', 'occupation-remove-link', kdf.profileData['profile-Occupation']);
+      if (validateAccountDetails(accountDetails)) {
+        KDF.customdata('cpe-create-account', this.id, true, true, accountDetails);
+      } else {
+        KDF.checkProgress();
+        window.scrollTo(0, 0);
+      }
+    });
   });
 
 // --- PAGE CHANGES --------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_pageChange')
   .on('_KDF_pageChange', function (event, kdf, currentpageid, targetpageid) {
     handlePageChangeEvent(event, kdf, currentpageid, targetpageid);
@@ -103,59 +100,27 @@ $('#dform_cpe_my_profile')
 
 // --- FIELD CHANGES -------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_fieldChange')
   .on('_KDF_fieldChange', function (event, kdf, field) {
     handleFieldChangeEvent(event, kdf, field);
   });
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_optionSelected')
   .on('_KDF_optionSelected', function (event, kdf, field, label, val) {
     handleOptionSelectedEvent(event, kdf, field, label, val);
   });
 
-$('#dform_cpe_my_profile')
-  .off('_KDF_rowSelected')
-  .on('_KDF_rowSelected', function (event, kdf, tableid, row) {
-
-  });
-
-// --- MAP ACTIONS ---------------------------------------------------------- \\
-
-$('#dform_cpe_my_profile')
-  .off('_KDF_mapReady')
-  .on('_KDF_mapReady', function (event, kdf, type, name, map, positionLayer, markerLayer, marker, projection) {
-    handleMapReadyEvent(event, kdf, type, name, map, positionLayer, markerLayer, marker, projection);
-  });
-
-$('#dform_cpe_my_profile')
-  .off('_KDF_mapClicked')
-  .on('_KDF_mapClicked', function (event, kdf, type, name, map, positionLayer, markerLayer, marker, lat, lon, plat, plon) {
-    handleMapClickEvent(event, kdf, type, name, map, positionLayer, markerLayer, marker, lat, lon, plat, plon);
-  });
-
-$('#dform_cpe_my_profile')
-  .off('_Selected_Layer')
-  .on('_Selected_Layer', function (event, kdf, layerName, layerAttributes) {
-    handleSelectedMapLayerEvent(event, kdf, layerName, layerAttributes);
-  });
-
-$("#dform_cpe_my_profile")
-  .off("_KDF_clearAttribute")
-  .on("_KDF_clearAttribute", function (event, kdf) {
-    handleClearMapFieldsEvent(event, kdf);
-  });
-
 // --- OBJECT --------------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_objectidSet')
   .on('_KDF_objectidSet', function (event, kdf, type, id) {
     handleObjectIdSet(event, kdf, type, id);
   });
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_objectdataLoaded')
   .on('_KDF_objectdataLoaded', function (event, kdf, response, type, id) {
     handleObjectIdLoaded(event, kdf, response, type, id);
@@ -163,14 +128,17 @@ $('#dform_cpe_my_profile')
 
 // --- CUSTOM ACTIONS ------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_custom')
   .on('_KDF_custom', function (event, kdf, response, action, actionedby) {
     handleSuccessfulAction(event, kdf, response, action, actionedby);
-    console.log(action, response);
+
+    if (action === "cpe-create-account") {
+      KDF.gotoPage('registered', true, true, false);
+    }
   });
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_customError')
   .on('_KDF_customError', function (event, customaction, xhr, settings, thrownError) {
     handleFailedAction(event, customaction, xhr, settings, thrownError);
@@ -178,13 +146,13 @@ $('#dform_cpe_my_profile')
 
 // --- SAVE FORM ------------------------------------------------------------ \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_save')
   .on('_KDF_save', function (event, kdf) {
     handleFormSave(event, kdf);
   });
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_saveError')
   .on('_KDF_saveError', function (event, kdf) {
     handleFailedSave();
@@ -192,7 +160,7 @@ $('#dform_cpe_my_profile')
 
 // --- COMPLETE FORM -------------------------------------------------------- \\
 
-$('#dform_cpe_my_profile')
+$('#dform_cpe_create_account')
   .off('_KDF_complete')
   .on('_KDF_complete', function (event, kdf) {
     handleFomComplate(event, kdf);
--- main.js
+++ main.js
@@ -878,132 +878,6 @@ function handleOnReadyEvent(_, kdf) {
     checkAddressHasBeenSet();
   });
 
-  function checkAddressHasBeenSet(action = "next page") {
-    const currentPageId = getCurrentPageId();
-    const selectedAddressSpan = document.querySelector(`#${currentPageId} #selected-address`);
-    const fullAddress = document.querySelector(
-      `#${currentPageId} input[data-customalias="fullAddress"]`
-    );
-    const fullAddressHasValue = KDF.getVal(fullAddress.name) ? true : false;
-    const siteName = document.querySelector(
-      `#${currentPageId} input[data-customalias="siteName"]`
-    );
-    const siteCode = document.querySelector(
-      `#${currentPageId} input[data-customalias="siteCode"]`
-    );
-    if (fullAddressHasValue) {
-      if (siteName && siteCode) {
-        const siteNameHasValue = KDF.getVal(siteName.name) ? true : false;
-        const siteCodeHasValue = KDF.getVal(siteCode.name) ? true : false;
-        const validSiteCode = acceptGMSites
-          ? true
-          : KDF.getVal(siteCode.name).startsWith("344")
-            ? true
-            : false;
-        if (siteNameHasValue && siteCodeHasValue && validSiteCode) {
-          if (action === "submit") {
-            KDF.gotoPage("complete", true, true, false);
-          } else {
-            KDF.gotoNextPage();
-          }
-        } else {
-          const errorMessage = acceptGMSites
-            ? defaultSelectedAddressMessage
-            : "Choose a location on the public highway";
-          if (selectedAddressSpan) {
-            selectedAddressSpan.textContent = errorMessage;
-            selectedAddressSpan.classList.add('dform_validationMessage');
-            selectedAddressSpan.style.display = 'block';
-          }
-          $("#map_container").addClass("map_container_error");
-          // if ($("#map_error").length == "0") {
-          //   $("#dform_widget_html_ahtm_map_container").prepend(
-          //     `<div id="map_error" class="dform_validationMessage" style="display: block;">${errorMessage}</div>`
-          //   );
-          // }
-          // KDF.setVal("ahtm_map_location_error", errorMessage);
-          // KDF.showWidget("ahtm_map_location_error");
-        }
-      } else {
-        if (action === "submit") {
-          KDF.gotoPage("complete", true, true, false);
-        } else {
-          KDF.gotoNextPage();
-        }
-      }
-    } else {
-      const mapElement = document.querySelector(`#${currentPageId} .map-container`);
-
-      // Check if the map element exists on the page
-      if (mapElement) {
-        const detailsElement = mapElement.querySelector('.details-accordion');
-
-        // Check if the map accordion is open
-        if (detailsElement && detailsElement.hasAttribute('open')) {
-
-
-
-          // const isMapContainerVisible = $("#map_container").is(":visible");
-          // if (isMapContainerVisible) {
-          const errorMessage = acceptGMSites
-            ? defaultSelectedAddressMessage
-            : "Choose a location on the public highway";
-          if (selectedAddressSpan) {
-            selectedAddressSpan.textContent = errorMessage;
-            selectedAddressSpan.classList.add('dform_validationMessage');
-            selectedAddressSpan.style.display = 'block';
-          }
-          $("#map_container").addClass("map_container_error");
-          // if ($("#map_error").length == "0") {
-          //   $("#dform_widget_html_ahtm_map_container").prepend(
-          //     `<div id="map_error" class="dform_validationMessage" style="display: block;">${errorMessage}</div>`
-          //   );
-          // }
-          // KDF.setVal("ahtm_map_location_error", errorMessage);
-          // KDF.showWidget("ahtm_map_location_error");
-        }
-      } else {
-        const searchResult = document.querySelector(
-          `#${currentPageId} select[data-customalias="searchResult"]`
-        );
-
-        const isSearchResultVisible = searchResult.offsetParent !== null;
-        if (isSearchResultVisible) {
-          const searchResultContainer = searchResult.closest('.dform_widget_field');
-          const validationMessage = searchResultContainer?.querySelector('.dform_validationMessage');
-          const selectedValue = searchResult.value;
-          let message = "Select the address";
-
-          if (selectedValue !== '' && selectedValue !== 'Please select...') {
-            const message = "Click use this address";
-          }
-          if (validationMessage) {
-            validationMessage.style.display = "block";
-            validationMessage.textContent = message;
-          }
-          searchResult.classList.add('dform_fielderror');
-        } else {
-          const postcode = document.querySelector(
-            `#${currentPageId} input[data-customalias="postcode"]`
-          );
-          const postcodeContainer = postcode?.closest('.dform_widget_field');
-          const validationMessage = postcodeContainer?.querySelector('.dform_validationMessage');
-          const postcodeHasValue = postcode ? KDF.getVal(postcode.name) : false;
-          let message = "Enter the postcode";
-          if (postcodeHasValue) {
-            message = "Click find address";
-          }
-          if (validationMessage) {
-            validationMessage.style.display = "block";
-            validationMessage.textContent = message;
-          }
-          postcode?.classList.add('dform_fielderror');
-        }
-
-      }
-    }
-  }
-
   // --- HANDLE CUSTOM DATE ------------------------------------------------ \\
 
   $(`.date-field`).find(".dform_validationMessage").hide();
@@ -3516,6 +3390,117 @@ function closeCase() {
   KDF.customdata("close-case", "_KDF_complete", true, true, {
     caseNote: `${KDF.getVal("sel_closure_reason")}: ${noteDetails}`,
   });
+}
+
+// --- ADDRESS FUNCTIONS ---------------------------------------------------- \\
+
+function checkAddressHasBeenSet(action = "next page") {
+  const currentPageId = getCurrentPageId();
+  const selectedAddressSpan = document.querySelector(`#${currentPageId} #selected-address`);
+  const fullAddress = document.querySelector(
+    `#${currentPageId} input[data-customalias="fullAddress"]`
+  );
+  const fullAddressHasValue = KDF.getVal(fullAddress.name) ? true : false;
+  const siteName = document.querySelector(
+    `#${currentPageId} input[data-customalias="siteName"]`
+  );
+  const siteCode = document.querySelector(
+    `#${currentPageId} input[data-customalias="siteCode"]`
+  );
+  if (fullAddressHasValue) {
+    if (siteName && siteCode) {
+      const siteNameHasValue = KDF.getVal(siteName.name) ? true : false;
+      const siteCodeHasValue = KDF.getVal(siteCode.name) ? true : false;
+      const validSiteCode = acceptGMSites
+        ? true
+        : KDF.getVal(siteCode.name).startsWith("344")
+          ? true
+          : false;
+      if (siteNameHasValue && siteCodeHasValue && validSiteCode) {
+        if (action === "submit") {
+          KDF.gotoPage("complete", true, true, false);
+        } else {
+          KDF.gotoNextPage();
+        }
+      } else {
+        const errorMessage = acceptGMSites
+          ? defaultSelectedAddressMessage
+          : "Choose a location on the public highway";
+        if (selectedAddressSpan) {
+          selectedAddressSpan.textContent = errorMessage;
+          selectedAddressSpan.classList.add('dform_validationMessage');
+          selectedAddressSpan.style.display = 'block';
+        }
+        $("#map_container").addClass("map_container_error");
+      }
+    } else {
+      if (action === "submit") {
+        KDF.gotoPage("complete", true, true, false);
+      } else {
+        KDF.gotoNextPage();
+      }
+    }
+  } else {
+    const mapElement = document.querySelector(`#${currentPageId} .map-container`);
+
+    // Check if the map element exists on the page
+    if (mapElement) {
+      const detailsElement = mapElement.querySelector('.details-accordion');
+
+      // Check if the map accordion is open
+      if (detailsElement && detailsElement.hasAttribute('open')) {
+
+        // const isMapContainerVisible = $("#map_container").is(":visible");
+        const errorMessage = acceptGMSites
+          ? defaultSelectedAddressMessage
+          : "Choose a location on the public highway";
+        if (selectedAddressSpan) {
+          selectedAddressSpan.textContent = errorMessage;
+          selectedAddressSpan.classList.add('dform_validationMessage');
+          selectedAddressSpan.style.display = 'block';
+        }
+        $("#map_container").addClass("map_container_error");
+      }
+    } else {
+      const searchResult = document.querySelector(
+        `#${currentPageId} select[data-customalias="searchResult"]`
+      );
+
+      const isSearchResultVisible = searchResult.offsetParent !== null;
+      if (isSearchResultVisible) {
+        const searchResultContainer = searchResult.closest('.dform_widget_field');
+        const validationMessage = searchResultContainer?.querySelector('.dform_validationMessage');
+        const selectedValue = searchResult.value;
+        let message = "Select the address";
+
+        if (selectedValue !== '' && selectedValue !== 'Please select...') {
+          const message = "Click use this address";
+        }
+        if (validationMessage) {
+          validationMessage.style.display = "block";
+          validationMessage.textContent = message;
+        }
+        searchResult.classList.add('dform_fielderror');
+      } else {
+        const postcode = document.querySelector(
+          `#${currentPageId} input[data-customalias="postcode"]`
+        );
+        const postcodeContainer = postcode?.closest('.dform_widget_field');
+        const validationMessage = postcodeContainer?.querySelector('.dform_validationMessage');
+        const postcodeHasValue = postcode ? KDF.getVal(postcode.name) : false;
+        let message = "Enter the postcode";
+        if (postcodeHasValue) {
+          message = "Click find address";
+        }
+        if (validationMessage) {
+          validationMessage.style.display = "block";
+          validationMessage.textContent = message;
+        }
+        postcode?.classList.add('dform_fielderror');
+      }
+
+    }
+  }
 }
 
 // --- MAP FUNCTIONS -------------------------------------------------------- \\
