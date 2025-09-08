7d52ccedc81a9d954d90cf0cce4bc103ad7ffd1c
elliott.griffiths.86 <elliott.griffiths.86@gmail.com>
2025-09-08 17:49:20 +0000

    testing

--- main.js
+++ main.js
@@ -2118,7 +2118,11 @@ function displayBackButton(show) {
       controlContainer.style.display = "flex";
       backButton.style.display = "flex";
     } else {
-      controlContainer.style.display = "none";
+      if (pageName === "complete" && kdf.form.complete === "Y") {
+        controlContainer.style.display = "flex";
+      } else {
+        controlContainer.style.display = "none";
+      }
       backButton.style.display = "none";
     }
   } else {
